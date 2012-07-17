f5110fd1174ffccb142a80afba82bff2cdd8250f
Michael Alexander <beefsack@gmail.com>
2012-07-17 16:51:52 +0800

    Added miniand configure script.

---
+++ tools/android/depends/miniand-configure.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./configure --with-toolchain=/home/beefsack/Development/xbmc-android-toolchain/android-9 --with-sdk=/home/beefsack/Development/android-sdk-linux --with-ndk=/home/beefsack/Development/android-ndk-r7-crystax-5.beta2 --with-staging=/home/beefsack/Development/xbmc-android-staging --with-tarballs=/home/beefsack/Development/xbmc-android-tarballs
